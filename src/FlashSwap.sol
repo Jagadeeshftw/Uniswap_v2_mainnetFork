e8707552c30aebb058931cc7f8fa3e52a0f8fcaf
Jagadeeshftw <jagadeesh26062002@gmail.com>
2024-10-25 22:51:08 +0530

    Add Arbitrage test

--- src/FlashSwap.sol
+++ src/FlashSwap.sol
@@ -1,41 +1,89 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity >=0.8.5;
 
+import {console2} from "forge-std/Test.sol";
 import {IUniswapV2Pair} from "../src/interfaces/IUniswapV2Pair.sol";
 import {IERC20} from "../src/interfaces/IERC20.sol";
+import {IUniswapV2Router02} from "../src/interfaces/IUniswapV2Router02.sol";
 
 contract TestFlashSwap {
-    IUniswapV2Pair private immutable pair;
+    IUniswapV2Pair private immutable uniswapPair;
     address private immutable token0;
     address private immutable token1;
 
-    constructor(address _pair) {
-        pair = IUniswapV2Pair(_pair);
-        token0 = pair.token0();
-        token1 = pair.token1();
+    struct SwapParams {
+        // Router to execute first swap - tokenIn for tokenOut
+        address router0;
+        // Router to execute second swap - tokenOut for tokenIn
+        address sushiswapRouter;
+        // Token in of first swap
+        address tokenIn;
+        // Token out of first swap
+        address tokenOut;
+        // Amount in for the first swap
+        uint256 amountIn;
+        // Revert the arbitrage if profit is less than this minimum
+        uint256 minProfit;
     }
 
-    function flashSwap(address token, uint256 amount) public {
-        (uint256 amount0, uint256 amount1) = token == token0 ? (amount, uint256(0)) : (uint256(0), amount);
+    constructor(address _pair1) {
+        uniswapPair = IUniswapV2Pair(_pair1);
+        token0 = uniswapPair.token0();
+        token1 = uniswapPair.token1();
+    }
 
-        bytes memory data = abi.encode(token, msg.sender);
+    function flashSwap(address token, SwapParams params) public {
+        // Log initial balance of the contract
+        uint256 initialBalance = IERC20(token).balanceOf(address(this)) / 1e18;
+        console2.log("Contract balance before borrowing:", initialBalance);
 
-        pair.swap({amount0Out: amount0, amount1Out: amount1, to: address(this), data: data});
+        // Determine which token to borrow
+        (uint256 amount0, uint256 amount1) = token == token0 ? (params.amountIn, uint256(0)) : (uint256(0), params.amountIn);
+
+        // Encode data to pass along with the swap
+        bytes memory data = abi.encode(token, msg.sender, params);
+
+        // Initiate the flash swap
+        uniswapPair.swap({amount0Out: amount0, amount1Out: amount1, to: address(this), data: data});
     }
 
     function uniswapV2Call(address sender, uint256 amount0, uint256 amount1, bytes calldata data) external {
-        require(msg.sender == address(pair), "not the pair contract");
-        require(sender == address(this), "sender should be equal to this contract");
+        require(msg.sender == address(uniswapPair), "Caller is not the pair contract");
+        require(sender == address(this), "Sender should be this contract");
 
-        (address token, address caller) = abi.decode(data, (address, address));
+        // Decode the data to obtain token and caller information
+        (address token, address caller, params) = abi.decode(data, (address, address, SwapParams));
 
-        uint256 amount = token == token0 ? amount0 : amount1;
+        // Log balance after borrowing
+        uint256 postBorrowBalance = IERC20(token).balanceOf(address(this)) / 1e18;
+        console2.log("Contract balance after borrowing:", postBorrowBalance);
 
-        uint256 fee = ((amount * 3) / 997) + 1;
-        uint256 amountToRepay = amount + fee;
+        // Determine the amount borrowed
+        uint256 borrowedAmount = token == token0 ? amount0 : amount1;
+        
 
+        address[] memory path = new address[](2);
+        path[0] = DAI;
+        path[1] = WETH;
+
+        IERC20(token).approve(address(sushiswapRouter), type(uint256).max);
+        uint256[] memory amounts = sushiswapRouter.swapExactTokensForTokens(borrowedAmount, 0, path, address(this), block.timestamp);
+        
+        console2.log("amounts: %s", amounts[1]);
+        // Calculate the 0.3% fee required to repay the borrowed amount
+        uint256 fee = ((borrowedAmount * 3) / 997) + 1;
+        uint256 amountToRepay = borrowedAmount + fee;
+        console2.log("repay amount: %s", amountToRepay);
+        cosole2.log("amount that we got through arbitrage: %s", amounts[1]);
+
+        // Ensure the contract receives the fee from the caller
         IERC20(token).transferFrom(caller, address(this), fee);
-        IERC20(token).transfer(address(pair), amountToRepay);
+
+        // Repay the loan plus fee
+        IERC20(token).transfer(address(uniswapPair), amountToRepay);
+        
+        // Log final balance to confirm loan repayment
+        uint256 finalBalance = IERC20(token).balanceOf(address(this)) / 1e18;
+        console2.log("Contract balance after repayment:", finalBalance);
     }
 }
--- test/FlashSwap.t.sol
+++ test/FlashSwap.t.sol
@@ -1,21 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.5;
+
 import {Test, console2} from "forge-std/Test.sol";
 import {IERC20} from "../src/interfaces/IERC20.sol";
-import {IUniswapV2Router02} from
-    "../src/interfaces/IUniswapV2Router02.sol";
-import {
-    DAI,
-    UNISWAP_V2_ROUTER_02,
-    UNISWAP_V2_PAIR_DAI_WETH
-} from "../src/Constants.sol";
+import {IUniswapV2Router02} from "../src/interfaces/IUniswapV2Router02.sol";
+import {IUniswapV2Pair} from "../src/interfaces/IUniswapV2Pair.sol";
+import {DAI, WETH, UNISWAP_V2_ROUTER_02, UNISWAP_V2_PAIR_DAI_WETH, SUSHISWAP_V2_PAIR_DAI_WETH} from "../src/Constants.sol";
 import {TestFlashSwap} from "../src/FlashSwap.sol";
 
 contract FlashSwapTest is Test {
     IERC20 private constant dai = IERC20(DAI);
+    IERC20 private constant weth = IERC20(WETH);
 
-    IUniswapV2Router02 private constant router =
-        IUniswapV2Router02(UNISWAP_V2_ROUTER_02);
+    IUniswapV2Router02 private constant router = IUniswapV2Router02(UNISWAP_V2_ROUTER_02);
+    IUniswapV2Pair private constant uniswapPair = IUniswapV2Pair(UNISWAP_V2_PAIR_DAI_WETH);
+    IUniswapV2Pair private constant suchiswapPair = IUniswapV2Pair(SUSHISWAP_V2_PAIR_DAI_WETH);
     TestFlashSwap private flashSwap;
 
     address private constant user = address(100);
@@ -23,18 +22,57 @@ contract FlashSwapTest is Test {
     function setUp() public {
         flashSwap = new TestFlashSwap(UNISWAP_V2_PAIR_DAI_WETH);
 
-        deal(DAI, user, 10000 * 1e18);
+        deal(DAI, user, 30000 * 1e18);
+        deal(WETH, user, 1000 * 1e18);
         vm.prank(user);
         dai.approve(address(flashSwap), type(uint256).max);
     }
 
-    function test_flashSwap() public {
-        uint256 dai0 = dai.balanceOf(UNISWAP_V2_PAIR_DAI_WETH);
+    function executeWethToDaiSwap() public {
+        address[] memory path = new address[](2);
+        path[0] = WETH;
+        path[1] = DAI;
+        vm.prank(user);
+        weth.approve(address(router), type(uint256).max);
+
+        vm.prank(user);
+        router.swapExactTokensForTokens({
+            amountIn: 100 * 1e18,
+            amountOutMin: 1,
+            path: path,
+            to: user,
+            deadline: block.timestamp
+        });
+    }
+
+    function logPoolState(IUniswapV2Pair pair) public view {
+        console2.log(address(pair) == address(uniswapPair) ? "Uniswap pool": "Sushiswap pool");
+        (uint112 reserveDAI, uint112 reserveWETH,) = pair.getReserves();
+        console2.log("DAI in pool: %s", reserveDAI / 1e18);
+        console2.log("WETH in pool: %s", reserveWETH / 1e18);
+        console2.log("WETH/DAI price: %s", reserveDAI / reserveWETH);
+    }
+
+    function test_flashSwapDAI() public {
+        uint256 initialDAIBalance = dai.balanceOf(UNISWAP_V2_PAIR_DAI_WETH);
         vm.prank(user);
         flashSwap.flashSwap(DAI, 1e6 * 1e18);
-        uint256 dai1 = dai.balanceOf(UNISWAP_V2_PAIR_DAI_WETH);
+        uint256 finalDAIBalance = dai.balanceOf(UNISWAP_V2_PAIR_DAI_WETH);
 
-        console2.log("DAI fee", dai1 - dai0);
-        assertGe(dai1, dai0, "DAI balance of pair");
+        console2.log("DAI swap fee: %s", finalDAIBalance - initialDAIBalance);
+        assertGe(finalDAIBalance, initialDAIBalance, "DAI balance in pair decreased");
+    }
+
+    function test_executeArbitrage() public {
+        logPoolState(uniswapPair);
+        logPoolState(suchiswapPair);
+
+        // Reduce WETH price to create arbitrage opportunity
+        //executeWethToDaiSwap();
+
+        
+        vm.prank(user);
+        flashSwap.flashSwap(DAI, 7e6 * 1e18);
+        logPoolState(uniswapPair);
     }
 }
